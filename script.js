d675db09605ac70cec17133cffdadc71d679d024
Andres Soria <andres.zov@gmail.com>
2022-09-09 17:18:09 -0500

    Add events to pass the content of the operations to our computation function

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ class Calculator {
   constructor(previousTxtEl, currenTxtEl) {
     this.previousTxtEl = previousTxtEl;
     this.currenTxtEl = currenTxtEl;
+    this.clear();
   }
-  clear() {}
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
   delete() {}
-  appendNumber(number) {}
-  chooseOperation(operation) {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
   compute() {}
-  updateDisplay() {}
+  updateDisplay() {
+    this.currenTxtEl.innerText = this.currentOperand;
+    this.previousTxtEl.innerText = this.previousOperand;
+  }
 }
 
 const numberBtns = document.querySelectorAll("[data-number]");
@@ -18,3 +38,19 @@ const deleteBtn = document.querySelector("[data-delete]");
 const allClearBtn = document.querySelector("[data-all-clear]");
 const previousTxtEl = document.querySelector("[data-previous-operand]");
 const currenTxtEl = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(previousTxtEl, currenTxtEl);
+
+numberBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
